c7e7ab99d4875894580af3a2c6d87aa345314b2f
K Adam White <kadamwhite@users.noreply.github.com>
2020-06-06 00:10:14 -0400

    Add plugin file

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Propose Draft Date
+ * Description: Permit contributing authors to suggest a date for a draft post which takes effect on publish.
+ * Version: 0.1
+ * Author: K. Adam White
+ * Author URI: https://www.kadamwhite.com
+ * License:     GPL-2.0+
+ */
+declare( strict_types=1 );
+
+namespace ProposeDraftDate;
+
+// Register custom meta values.
+require_once __DIR__ . '/inc/meta.php';
+Meta\setup();
+
+// Load scripts & styles.
+require_once __DIR__ . '/inc/asset-loader.php';
+require_once __DIR__ . '/inc/scripts.php';
+Scripts\setup();

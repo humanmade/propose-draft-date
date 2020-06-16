04f86d3f6696441ad9569799344c97876099cc0d
tfrommen <info@tfrommen.de>
2020-06-16 08:35:10 +0200

    Test bootstrap: Extract and align base directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,15 +7,16 @@
 
 declare( strict_types=1 );
 
+$base_dir = dirname( __DIR__ );
+
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available.
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+require_once $base_dir . '/vendor/autoload.php';
 
 // Give access to tests_add_filter() function.
 require '/wp-phpunit/includes/functions.php';
 
 // Load in our custom files.
-require dirname( dirname( __FILE__ ) ) . '/inc/meta.php';
+require $base_dir . '/inc/meta.php';
 
 // Start up the WP testing environment.
 require '/wp-phpunit/includes/bootstrap.php';
-
